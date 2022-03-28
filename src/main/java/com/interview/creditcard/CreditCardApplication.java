545028aea65f7057d2900c50c7d9988aed06681b
agangwal <arpit.gangwalindore@gmail.com>
2022-03-28 08:48:15 +0100

    Added Cross Origin

--- src/main/java/com/interview/creditcard/CreditCardApplication.java
+++ src/main/java/com/interview/creditcard/CreditCardApplication.java
@@ -1,13 +1,27 @@
 package com.interview.creditcard;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class CreditCardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CreditCardApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer configure(){
+		return new WebMvcConfigurer(){
+			@Override
+			public void addCorsMappings(CorsRegistry corsRegistry){
+				corsRegistry.addMapping("/*").allowedOrigins("*");
+			}
+		};
+	}
 }
